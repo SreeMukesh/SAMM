5d273397d87ed40551bc0c649db34fa9fa60a7b7
SreeMukesh <sree_mukesh@yahoo.co.in>
2025-05-27 16:04:46 +0530

    Add files via upload

---
+++ seat-booking/src/App.jsx
@@ -0,0 +1,9 @@
+import BusSeatSelector from './BusSeatSelector'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <BusSeatSelector />
+    </div>
+  )
+}
