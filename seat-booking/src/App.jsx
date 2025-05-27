550ecd492ad38c455cb31c4d60fb9fda5fe24108
SreeMukesh <sree_mukesh@yahoo.co.in>
2025-05-27 16:16:07 +0530

    Delete seat-booking directory

--- seat-booking/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import BusSeatSelector from './BusSeatSelector'
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-gray-100 p-4">
-      <BusSeatSelector />
-    </div>
-  )
-}
